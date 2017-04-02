776eccb194c26b1073a7b294fa625093e1303f57
scott coates <scoarescoare@gmail.com>
2017-04-02 13:08:00 -0500

    fix name

--- src/apps/music_discovery/event_handlers.py
+++ src/apps/music_discovery/event_handlers.py
@@ -8,11 +8,11 @@ from src.domain.request.events import RequestSubmitted1, AlbumAddedToRequest1, P
 def execute_assignment_batch_1(**kwargs):
   event = kwargs['event']
 
-  artist_ids = event.data['artist_names']
+  artist_names = event.data['artist_names']
   r_id = kwargs['aggregate_id']
 
-  for artist_id in artist_ids:
-    tasks.discover_music_for_request_task.delay(r_id, artist_id)
+  for artist_name in artist_names:
+    tasks.discover_music_for_request_task.delay(r_id, artist_name)
 
 
 # todo move to reequest domain
@@ -26,6 +26,17 @@ def add_album_1(**kwargs):
 
   discover_tracks_task = tasks.discover_tracks_for_album_task.delay(album_id, artist_id)
   tasks.update_request_playlist_task.delay(r_id, album_id, depends_on=discover_tracks_task)
+
+
+@receiver(PlaylistRefreshedWithTracks1.event_signal)
+def playlist_refreshed_1(**kwargs):
+  event = kwargs['event']
+
+  provider_type = event.data['provider_type']
+  external_id = event.data['external_id']
+  track_ids = event.data['track_ids']
+
+  tasks.update_playlist_with_tracks_task.delay(external_id, track_ids)
 
 
 @receiver(PlaylistRefreshedWithTracks1.event_signal)
