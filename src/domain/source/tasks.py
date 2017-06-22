b59a4a0496ec70aaea2b394a3a2bec61381de200
scott coates <scoarescoare@gmail.com>
2017-06-21 20:34:17 -0700

    schedule to check artist id

--- src/domain/source/tasks.py
+++ src/domain/source/tasks.py
@@ -13,7 +13,6 @@ logger = logging.getLogger(__name__)
 @job(queue='default')
 def create_source_lookup_schedule_task(provider_type, source_type, attrs):
   source_lookup_schedule_task.delay(provider_type, source_type, attrs)
-  pass
 
 
 @job(queue='default', schedule=periodic(days=1, start_date=timezone.now() + relativedelta(days=1)))
@@ -21,10 +20,8 @@ def source_lookup_schedule_task(provider_type, source_type, attrs):
   # use tiger code to create periodic task
   # this task is going to call a service --> and that service will interact w/ the source to get new data
   source_lookup_task(provider_type, source_type, attrs)
-  pass
 
 
 @job(queue='default')
 def source_lookup_task(provider_type, source_type, attrs):
   source_lookup(provider_type, source_type, attrs)
-  pass
